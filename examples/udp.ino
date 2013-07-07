d3cdd88734a028562225eed4ce066b0b78941298
Tim Cameron Ryan <id@timryan.org>
2013-07-07 18:54:52 -0400

    Examples

---
+++ examples/udp.ino
@@ -0,0 +1,92 @@
+#include <SPI.h>
+#include <cc3000.h>
+
+int status = WL_IDLE_STATUS;
+char ssid[] = "...";                 // your network SSID (name) 
+char pass[] = "...";                 // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                    // your network key Index number (needed only for WEP)
+
+unsigned int localPort = 2390;       // local port to listen on
+
+char inBuffer[255];                  // buffer to hold incoming packet
+char outBuffer[] = "acknowledged";   // a string to send back
+
+WiFiUDPQueue Udp;
+
+void setup ()
+{
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  Serial.println(F("Send anything."));
+  while (!Serial.available()) { continue; }
+  
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println(F("WiFi shield not present")); 
+    // don't continue:
+    while(true);
+  } 
+  
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) { 
+    Serial.print(F("Attempting to connect to SSID: "));
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, pass);
+  
+    // wait 10 seconds for connection:
+    // delay(10000);
+  } 
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+  
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  Udp.begin(localPort);  
+}
+
+void loop ()
+{    
+  // if there's data available, read a packet
+  if (Udp.available())
+  {   
+    // read the packet into packetBufffer
+    int len = Udp.receive(packetBuffer,255);
+    if (len >0) packetBuffer[len]=0;
+    
+    Serial.print("Received packet from ");
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+    
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.send(Udp.remoteIP(), Udp.remotePort(), ReplyBuffer, strlen(ReplyBuffer));
+   }
+}
+
+
+void printWifiStatus ()
+{
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
