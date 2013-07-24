c412dfd51ec3fea54aee6d1395799cd273e50116
jialiya <jialiya.huang@students.olin.edu>
2013-07-24 10:10:34 -0400

    added driver patch

---
+++ examples/driver_patch/driver_patch.ino
@@ -0,0 +1,34 @@
+#include <SPI.h>
+#include <CC3000.h>
+
+/* Do the driver patch before the firmware patch
+ */
+
+int patched = 0;
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  Serial.println("Type 'X' to begin driver patch.");
+  Serial.println("Do the driver patch before the firmware patch.");
+
+}
+
+void loop() {
+  if ( Serial.available() )
+  { 
+    char c = toupper(Serial.read());
+    
+    if (c == 'X' && patched == 0) {
+      patched = 1;
+      Serial.println("Beginning driver patch...");
+      driver_patch();
+      Serial.println("Done with driver patch");
+    } else if (c == 'X' && patched == 1){
+      Serial.println("Already patched");
+    }
+  }
+}
